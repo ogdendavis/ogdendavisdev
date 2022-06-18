cc57499835d689c579660577e1b85d0b3d3cc613
Lucas Ogden-Davis <lucas@ogdendavis.com>
2022-06-18 10:19:00 -0400

    Add Play content

--- src/blocks/Play/Play.tsx
+++ src/blocks/Play/Play.tsx
@@ -4,6 +4,48 @@ export const Play = () => {
   return (
     <PageSection darkMode>
       <h2>Play</h2>
+      <p>
+        When I'm not writing some sweet JavaScript or trouble-shooting a
+        stubborn bit of CSS, I can often be found reading a novel (Terry
+        Pratchett is a favorite of mine), getting my sweat on (I used to own a
+        gym!), or playing with my dogs.
+      </p>
+      <p>
+        If you're in Brooklyn, you can often find me grabbing a coffee from{" "}
+        <a
+          href="https://www.instagram.com/brooklynperkcoffee/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Brooklyn Perk
+        </a>{" "}
+        or wandering around{" "}
+        <a
+          href="https://www.prospectpark.org/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Prospect Park
+        </a>{" "}
+        (especially during off-leash hours!). On the weekends, my wife and I
+        like to climb at{" "}
+        <a
+          href="https://gowanus.thecliffsclimbing.com/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          The Cliffs
+        </a>{" "}
+        and then have a burger and a beer at{" "}
+        <a
+          href="https://threesbrewing.com/locations/gowanus/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Three's Brewing
+        </a>
+        .
+      </p>
     </PageSection>
   );
 };
