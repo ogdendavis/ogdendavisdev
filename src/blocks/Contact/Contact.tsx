e481ed0990f3e498f6447eb1a0b656e7c862975e
Lucas Ogden-Davis <lucas@ogdendavis.com>
2022-06-19 11:08:57 -0400

    Add content to Contact section

--- src/blocks/Contact/Contact.tsx
+++ src/blocks/Contact/Contact.tsx
@@ -1,3 +1,16 @@
+import { PageSection } from "../../components/PageSection";
+
 export const Contact = () => {
-  return <div>I am the contact section</div>;
+  return (
+    <PageSection>
+      <h2>Contact</h2>
+      <p>
+        If you're a stranger on the internet who wants to get in touch with me,
+        then your best shot is email:{" "}
+        <a href="mailto:lucas@ogdendavis.com?subject=Hi, I'm a stranger from the internet">
+          lucas@ogdendavis.com
+        </a>
+      </p>
+    </PageSection>
+  );
 };
