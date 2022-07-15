c7c7dc328962e0fa1aff447fc36ba0b57dcf1986
Lucas Ogden-Davis <lucas@ogdendavis.com>
2022-07-15 13:29:42 -0400

    WIP: Set up About section to show portrait

--- src/blocks/About/About.tsx
+++ src/blocks/About/About.tsx
@@ -1,28 +1,39 @@
+import styled from "styled-components/macro";
+
 import { PageSection } from "../../components/PageSection";
 
 import { AboutPortrait } from "./AboutPortrait";
 
+const Half = styled.div`
+  display: inline-block;
+  width: 50%;
+`;
+
 export const About = () => {
   return (
     <PageSection darkMode>
-      <h2>About Me</h2>
-      <p>
-        I'm a full-stack software developer, urban hiker, and voracious reader.
-        I work as the Founding Engineer for{" "}
-        <a href="https://beepboop.us/" target="_blank" rel="noreferrer">
-          Beepboop
-        </a>
-        , one of New York's{" "}
-        <a
-          href="https://www.edutech.coffee/post/101-top-edtech-startups-companies-new-york"
-          target="_blank"
-          rel="noreferrer"
-        >
-          top edTech startups
-        </a>
-        , and I live in Brooklyn with my wife and our two dogs.
-      </p>
-      <AboutPortrait />
+      <Half>
+        <h2>About Me</h2>
+        <p>
+          I'm a full-stack software developer, urban hiker, and voracious
+          reader. I work as the Founding Engineer for{" "}
+          <a href="https://beepboop.us/" target="_blank" rel="noreferrer">
+            Beepboop
+          </a>
+          , one of New York's{" "}
+          <a
+            href="https://www.edutech.coffee/post/101-top-edtech-startups-companies-new-york"
+            target="_blank"
+            rel="noreferrer"
+          >
+            top edTech startups
+          </a>
+          , and I live in Brooklyn with my wife and our two dogs.
+        </p>
+      </Half>
+      <Half>
+        <AboutPortrait />
+      </Half>
     </PageSection>
   );
 };
--- src/blocks/About/AboutPortrait.tsx
+++ src/blocks/About/AboutPortrait.tsx
@@ -1,15 +1,55 @@
+import styled from "styled-components/macro";
+
 import { ReactComponent as LucasSVG } from "../../assets/lucas.svg";
 import { ReactComponent as KimSVG } from "../../assets/kim.svg";
 import { ReactComponent as FiggySVG } from "../../assets/figgy.svg";
 import { ReactComponent as CedricSVG } from "../../assets/cedric.svg";
 
+const Frame = styled.div`
+  border-bottom: 2px solid ${({ theme }) => theme.colors.brightYellow};
+`;
+
+const FiggyContainer = styled.div`
+  display: inline-block;
+  position: relative;
+  left: 5%;
+  width: 15%;
+`;
+
+const LucasContainer = styled.div`
+  display: inline-block;
+  width: 42.5%;
+`;
+
+const CedricContainer = styled.div`
+  display: inline-block;
+  position: relative;
+  right: 5%;
+  width: 15%;
+`;
+
+const KimContainer = styled.div`
+  display: inline-block;
+  position: relative;
+  right: 5%;
+  width: 27.5%;
+`;
+
 export const AboutPortrait = () => {
   return (
-    <div>
-      <FiggySVG />
-      <LucasSVG />
-      <CedricSVG />
-      <KimSVG />
-    </div>
+    <Frame>
+      <FiggyContainer>
+        <FiggySVG />
+      </FiggyContainer>
+      <LucasContainer>
+        <LucasSVG />
+      </LucasContainer>
+      <CedricContainer>
+        <CedricSVG />
+      </CedricContainer>
+      <KimContainer>
+        <KimSVG />
+      </KimContainer>
+    </Frame>
   );
 };
